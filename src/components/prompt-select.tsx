7283a7920abc7b33115c7e87d8cdc515b676f7b6
Michael C. Strahl <michaelstrahl.sm@gmail.com>
2023-09-14 01:02:30 -0300

    connected backend

---
+++ src/components/prompt-select.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
+import { api } from '@/lib/axios'
+
+interface PromptsProps {
+  id: string
+  title: string
+  template: string
+}
+
+interface PromptsSelectProps {
+  onPromptSelected: (template: string) => void
+}
+
+export function PromptSelect({ onPromptSelected }: PromptsSelectProps) {
+  const [prompts, setPrompts] = useState<PromptsProps[] | null>(null)
+
+  useEffect(() => {
+    api.get('/prompts').then((response) => {
+      setPrompts(response.data)
+    })
+  }, [])
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts?.find((prompt) => prompt.id === promptId)
+
+    if (!selectedPrompt) return
+
+    onPromptSelected(selectedPrompt.template)
+  }
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map((prompt) => {
+          return (
+            <SelectItem key={prompt.id} value={prompt.id}>
+              {prompt.title}
+            </SelectItem>
+          )
+        })}
+      </SelectContent>
+    </Select>
+  )
+}
